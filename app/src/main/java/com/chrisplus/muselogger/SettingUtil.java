b01986759d2a2165eb439daa49fe2d1671a36399
Shiqi Jiang <chrisplus.jiang@gmail.com>
2015-07-01 14:27:38 +0800

    ADD simple setting utils

---
+++ app/src/main/java/com/chrisplus/muselogger/SettingUtil.java
@@ -0,0 +1,24 @@
+package com.chrisplus.muselogger;
+
+import android.content.Context;
+import android.preference.PreferenceManager;
+
+/**
+ * Created by chrisplus on 7/1/15.
+ */
+public class SettingUtil {
+
+    public final static String getRecordPath(Context context) {
+        return getPreferenceString(context, context.getString(R.string.pref_record_path),
+                context.getString(R.string.pref_default_record_path));
+    }
+    
+
+    private static String getPreferenceString(Context context, String key, String defaultString) {
+        return PreferenceManager.getDefaultSharedPreferences(context).getString(key, defaultString);
+    }
+
+    private static Boolean getPreferenceBoolean(Context context, String key, Boolean defaultValue) {
+        return PreferenceManager.getDefaultSharedPreferences(context).getBoolean(key, defaultValue);
+    }
+}
