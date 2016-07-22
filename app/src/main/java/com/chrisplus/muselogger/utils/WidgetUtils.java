00083eb717881a129c8dee5c9aaa0d0a03eafe8e
Shiqi Jiang <chrisplus.jiang@gmail.com>
2016-07-22 15:18:04 +0800

    ADD dialog builder

---
+++ app/src/main/java/com/chrisplus/muselogger/utils/WidgetUtils.java
@@ -0,0 +1,32 @@
+package com.chrisplus.muselogger.utils;
+
+import com.orhanobut.dialogplus.DialogPlus;
+import com.orhanobut.dialogplus.ListHolder;
+import com.orhanobut.dialogplus.OnItemClickListener;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by chrisplus on 22/7/16.
+ */
+public class WidgetUtils {
+    public static final String TAG = WidgetUtils.class.getSimpleName();
+
+    private WidgetUtils() {
+
+    }
+
+    public static final DialogPlus buildListDialog(BaseAdapter adapter, ListHolder listHolder,
+                                                   OnItemClickListener onItemClickListener, View
+                                                           headerView, Context activityContext) {
+
+        final DialogPlus dialogPlus = DialogPlus.newDialog(activityContext).setAdapter(adapter)
+                .setHeader(headerView).setContentHolder(listHolder).setOnItemClickListener
+                        (onItemClickListener).create();
+        return dialogPlus;
+    }
+
+
+}
